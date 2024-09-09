f2844e2f26a2850a2b7ca75d815696aba82c6928
yashjain8448 <jainyash8448@gmail.com>
2024-09-09 11:37:44 +0530

    Added First File

---
+++ Start.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Group 7 SEM LAB"<<endl; 
+    cout << "Yash Jain"<<endl;
+    cout << "Yashasvi Arya"<<endl;
+    cout << "Vivek Kumar"<<endl;
+    cout << "Yashika Gupta"<<endl;
+    return 0;
+}
